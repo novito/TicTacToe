facb1b16b14609922a837af8be4d5ab4130ab1bc
Joan Roig <roigarderiu@gmail.com>
2014-09-06 10:25:14 -0700

    Added initialize for HumanPlayer

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -1,2 +1,5 @@
 class HumanPlayer
+  def initialize(name)
+    raise ArgumentError, "Name can't be blank" if name.nil? || name.strip.empty?
+  end
 end
--- spec/human_player_spec.rb
+++ spec/human_player_spec.rb
@@ -4,5 +4,10 @@ describe HumanPlayer do
   let(:human_player) { create(:human_player, name: 'Hommer Simpson') }
 
   describe '#initialize' do
+    context 'when name is blank' do
+      it 'should raise an error' do
+        expect {HumanPlayer.new(' ')}.to raise_error(ArgumentError)
+      end
+    end
   end
 end
